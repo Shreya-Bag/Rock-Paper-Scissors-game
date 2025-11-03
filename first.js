888de3529c0097752af5fce907e12adbe2ba46d2
Shreya <shreyabag6@gmail.com>
2025-11-03 23:30:15 +0530

    Added images of player and their corresponding CSS and JS

--- first.js
+++ first.js
@@ -1,4 +1,6 @@
 const button = document.querySelector("button");
+const player_1 = document.getElementById("first");
+const player_2 = document.getElementById("second");
 
 const options = ["Rock","Paper","Scissor"];
 
@@ -16,12 +18,18 @@ button.addEventListener("click",()=>{
     let result = document.getElementById("result");
     if(player1.innerHTML=="Rock"&&player2.innerHTML=="Scissor"  || player1.innerHTML=="Paper"&&player2.innerHTML=="Rock" ||player1.innerHTML=="Scissor"&&player2.innerHTML=="Paper"){
         result.innerHTML= "Player 1 is Winner"
+        player_1.classList.add("player1")
+        player_2.classList.remove("player2")
     }
     else if(player1.innerHTML==player2.innerHTML){
         result.innerHTML = "Match is Draw";
+        player_1.classList.add("player1")
+        player_2.classList.add("player2")
     }
     else{
         result.innerHTML= "Player 2 is Winner"
+        player_2.classList.add("player2")
+        player_1.classList.remove("player1")
     }
     
 })
